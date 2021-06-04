d1011dc7127196277d8e1cc014fa5914bb8e315a
jonghak96 <jonghak96@outlook.com>
2021-06-04 22:16:37 +0900

    jQuery Day09

--- 06_02/js/script.js
+++ 06_02/js/script.js
@@ -30,36 +30,82 @@
 
         calendarFn : function() {
 
-            var $year = $(".year");
-            var $month = $(".month");
+            // var $year = $(".year");
+            // var $month = $(".month");
+            var today = new Date();
+            var year = null;
+            var month = null;
+            
+            getYearMonthFn();
+            mainCalendarFn();
+            
+            // 년월 구하기 함수
+            function getYearMonthFn() {
+                year = today.getFullYear();
+                month = today.getMonth();
+            }
 
             // 달력 메인 함수
             function mainCalendarFn() {
 
-                var today = new Date();
-                var year = today.getFullYear();
-                var month = today.getMonth()+1;
+                $(".year").text(year+"년");
+                $(".month").text((month+1)+"월");
 
-                $year.text(year+"년");
-                $month.text(month+"월");
-
-                // 이전 달의 끝날
-                // var prevMonth = new Date(year, month-2, -1);
-                // var prevMonth_lastDate = prevMonth.getDate();
-                // 달의 첫날
-                var thisMonth = new Date(year, month-1, 1);
-                var firstDay = thisMonth.getDay();
-                // 달의 끝날
-                var lastDate = new Date(year, month, 0).getDate();
-
-                for(var i=1;i<=lastDate;i++) {
-                    console.log(i);
-                    $("td").eq(firstDay).text(i);
-                    firstDay++;
+                var prevMonth_lastDate = new Date(year, month, 0).getDate();
+                var thisMonth_firstDay = new Date(year, month, 1).getDay();
+                var thisMonth_lastDate = new Date(year, month+1, 0).getDate();
+                
+                
+                var lastDate = prevMonth_lastDate;
+                for(let i=thisMonth_firstDay-1; i>=0; i--) {
+                    $("td").eq(i).text(lastDate--).css({color:"#555"});
                 }
 
+                var firstDay = thisMonth_firstDay;
+                for(let i=1; i<=thisMonth_lastDate; i++) {
+                    $("td").eq(firstDay++).text(i).css({color:"#fff"});
+                }
+
+                var nextDate = 1;
+                for(let i=(thisMonth_firstDay+thisMonth_lastDate); i<=42; i++) {
+                    $("td").eq(i).text(nextDate++).css({color:"#555"});
+                }
+
+                getTodayFn();
             }
-            mainCalendarFn();
+
+            // 오늘 표시 함수
+            function getTodayFn() {
+
+                var now = new Date();
+                var isNow = (now.getFullYear() == year) && (now.getMonth() == month);
+    
+                if(isNow) {
+                    // $("td").eq(now.getDay()).css({background:"royalblue", outline:"2px solid #222", outlineOffset: "-4px"});
+                    $("td").eq(now.getDay()).addClass("addToday");
+                } else {
+                    // $("td").eq(now.getDay()).css({background:"transparent", outline:"0px transparent"});
+                    $("td").eq(now.getDay()).removeClass("addToday");
+                }
+            }
+
+            $(".prevMonth").on({
+                click(e) {
+                    e.preventDefault();
+                    today.setMonth(today.getMonth()-1);
+                    getYearMonthFn();
+                    mainCalendarFn();
+                }
+            });
+
+            $(".nextMonth").on({
+                click(e) {
+                    e.preventDefault();
+                    today.setMonth(today.getMonth()+1);
+                    getYearMonthFn();
+                    mainCalendarFn();
+                }
+            });
 
         }
 
